cb43fae30c7333833ca6a69e574afa770dd58b80
Simon Bjerre <bjerrecs@protonmail.com>
2025-03-24 22:23:22 +0100

    setting up stages

---
+++ CDA_WEU.ps1
@@ -0,0 +1,47 @@
+﻿##
+## Import shared variables
+##
+$currentAzContext = Get-AzContext
+$subscriptionID=$currentAzContext.Subscription.Id
+$GalleryName = "nncd"
+$ResourceGroup = "nncd-automation"
+$location="NorwayEast"
+$imageDefName = "nncda-2503"
+$templateFilePath = $imageDefName+"-Template.json"
+$ADORepo = "https://raw.githubusercontent.com/bjerrecs/aibTemplates/refs/heads/main/"
+$replRegion2="NorwayWest"
+$runOutputName="winclientR01"
+
+New-AzGalleryImageDefinition -GalleryName $GalleryName `
+    -ResourceGroupName $ResourceGroup `
+    -Location $location `
+    -Name $imageDefName `
+    -OsState generalized `
+    -OsType Windows `
+    -Publisher 'DanofficeIT' `
+    -Offer 'Windows' `
+    -Sku 'Win11Mu'
+
+Invoke-WebRequest -Uri $($ADORepo+"win11ms.json") `
+    -OutFile $templateFilePath -UseBasicParsing
+
+(Get-Content -path $templateFilePath -Raw ) -replace '<subscriptionID>',$subscriptionID | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<rgName>',$ResourceGroup | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<runOutputName>',$runOutputName | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<imageDefName>',$imageDefName | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<sharedImageGalName>',$sigGalleryName | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<region1>',$location | Set-Content -Path $templateFilePath
+(Get-Content -path $templateFilePath -Raw ) -replace '<region2>',$replRegion2 | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$identityNameResourceId) | Set-Content -Path $templateFilePath
+
+
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup `
+    -TemplateFile $templateFilePath `
+    -Api-Version "2022-02-14" `
+    -imageTemplateName $imageDefName `
+    -svclocation $location
+
+Invoke-AzResourceAction -ResourceName $imageDefName+"1" `
+    -ResourceGroupName $ResourceGroup `
+    -ResourceType Microsoft.VirtualMachineImages/imageTemplates `
+    -Action Run
---
+++ setupv2.ps1
@@ -0,0 +1,34 @@
+﻿# Install Module Providers
+Install-Module -Name Az.ManagedServiceIdentity -Scope CurrentUser
+Install-Module -Name Az.ImageBuilder -Scope CurrentUser
+Install-Module -Name Az.Resources -Scope CurrentUser
+Install-Module -Name Az.Compute -Scope CurrentUser
+
+# Variables
+$currentAzContext = Get-AzContext
+$subscriptionID=$currentAzContext.Subscription.Id
+$ResourceGroup = "nncd-automation"
+$time = $(get-date -UFormat "%s").Split(',')[0]
+$Identity="AZUAI"+$time
+$RoleDefName="Azure Image Builder Image Def"+$time
+$Location="NorwayEast"
+$GalleryName= "nncd"
+
+
+New-AzResourceGroup -Name $ResourceGroup -Location $location
+New-AzUserAssignedIdentity -ResourceGroupName $ResourceGroup -Name $Identity -Location $Location
+$UAI = Get-AzUserAssignedIdentity -ResourceGroupName $ResourceGroup -Name $Identity
+
+$RoleImageCreationUrl="https://raw.githubusercontent.com/azure/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+$RoleImageCreationPath = "RoleImageCreation.json"
+Invoke-WebRequest -Uri $RoleImageCreationUrl -OutFile $RoleImageCreationPath -UseBasicParsing
+
+((Get-Content -path $RoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $RoleImageCreationPath
+((Get-Content -path $RoleImageCreationPath -Raw) -replace '<rgName>', $ResourceGroup) | Set-Content -Path $RoleImageCreationPath
+((Get-Content -path $RoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $RoleDefName) | Set-Content -Path $RoleImageCreationPath
+
+New-AzRoleDefinition -InputFile ./RoleImageCreation.json
+
+New-AzRoleAssignment -ObjectId $UAI.PrincipalId -RoleDefinitionName $RoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$ResourceGroup"
+
+New-AzGallery -GalleryName $GalleryName -ResourceGroupName $ResourceGroup -Location $location
