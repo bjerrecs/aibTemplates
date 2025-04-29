a4a4625b2e4899462361605f1f198ea543bcfcba
Simon Bjerre <bjerrecs@protonmail.com>
2025-04-29 12:25:43 +0200

    Update pkg.ps1

--- pkg.ps1
+++ pkg.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Packages = 'googlechrome', 'git', 'notepadplusplus'
+$Packages = '7Zip', 'adobereader', 'jmeter', 'maven', 'dwgtrueview', 'awscli', 'azure-cli', 'microsoftazurestorageexplorer', 'DBeaver', 'fiddler', 'filezilla', 'fslogix', 'git', 'googlechrome', 'javaruntime', 'MiniConda3', 'notepadplusplus', 'Postman', 'pspad', 'putty.install', 'Python3', 'rancher-desktop', 'Rdcman', 'Soapui', 'techsmithsnagit'
 
 ForEach ($PackageName in $Packages)
 {
